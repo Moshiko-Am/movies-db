fbf29957bef599c3233e07a40e9d43a39ad85f05
Moshiko-Start <moshiko14691@gmail.com>
2021-12-05 13:29:28 +0200

    Updates

---
+++ precache-manifest.bcaf899b58ca6a9b0c3dfc78180dad47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e8ed34b3f0959c83ef03a8fe362c4a",
+    "url": "/movies-db/index.html"
+  },
+  {
+    "revision": "bceb2d99958e64080670",
+    "url": "/movies-db/static/css/main.3d22e596.chunk.css"
+  },
+  {
+    "revision": "290aa4f4295c4e36967e",
+    "url": "/movies-db/static/js/2.33260d0b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies-db/static/js/2.33260d0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bceb2d99958e64080670",
+    "url": "/movies-db/static/js/main.07b9a9a0.chunk.js"
+  },
+  {
+    "revision": "a4107d7918d156ac9cc6",
+    "url": "/movies-db/static/js/runtime-main.d6077bd6.js"
+  }
+]);
